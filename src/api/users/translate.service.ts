0ae665341352bd6635a30806802ffbeafe3f7916
Maxoverking <avemax7@gmail.com>
2024-04-07 10:51:13 +0200

    add p5

--- src/api/users/translate.service.ts
+++ src/api/users/translate.service.ts
@@ -6,19 +6,22 @@ export class TranslateService {
   private readonly translate: Translate;
 
   constructor() {
+    const CREDENTIAL = JSON.parse(process.env.TRANSLATE_CREDENTIAL);
     this.translate = new Translate({
-      credentials: JSON.parse(process.env.TRANSLATE_CREDENTIAL),
-      projectId: JSON.parse(process.env.TRANSLATE_CREDENTIAL).project_id,
+      credentials: CREDENTIAL,
+      projectId: CREDENTIAL.project_id
     });
   }
 
-  async translateText(text: string): Promise<string> {
-    const lang = 'en';
-    const [translation] = await this.translate.translate(text, lang);
-    // console.log(`Text: ${text}`);
-    // console.log(`Translation: ${translation}`);
-    return translation;
-
+  async translateText(text: string, targetLanguage: string = 'en'): Promise<string> {
+    try {
+      const [translation] = await this.translate.translate(text, targetLanguage);
+      // console.log(`Text: ${text}`);
+      // console.log(`Translation: ${translation}`);
+      return translation;
+    } catch (error) {
+      throw error;
+    }
   }
 
 }
